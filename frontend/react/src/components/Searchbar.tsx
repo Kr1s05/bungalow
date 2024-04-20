59f9ac0494d322d214c09be882d48dcb162c18e0
Kr1s05 <kristianoreshkov04@gmail.com>
2024-04-20 15:16:11 +0300

    added tasks and categories for keywords on search bar

--- frontend/react/src/components/Searchbar.tsx
+++ frontend/react/src/components/Searchbar.tsx
@@ -3,28 +3,48 @@ import { Input } from "./ui/input";
 import { ChangeEvent, useState } from "react";
 
 type Category = "name" | "phone" | "email";
+type Keyword = { word: string; categories: Array<Category> };
 
 export default function Searchbar(props: { ClassName?: string }) {
-  const [state, setState] = useState({
+  const [state, setState] = useState<{
+    search: string;
+    keywords: Array<Keyword>;
+  }>({
     search: "",
     keywords: [],
   });
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     const search = e.target.value;
     const words = search.split(" ");
-    const keywords = words.map(getCategory);
-    setState((prevState) => ({ ...prevState, search }));
+    const keywords = words
+      .filter((word) => word != "" && word)
+      .map(getCategory);
+    console.log(keywords);
+    setState((prevState) => ({ ...prevState, search, keywords }));
   };
-  const getCategory = (
-    word: string
-  ): { word: string; categories: Array<Category> } => {
-    const result: { word: string; categories: Array<Category> } = {
+  const getCategory = (word: string): Keyword => {
+    const result: Keyword = {
       word,
-      categories: [],
+      categories: ["email", "name", "phone"],
     };
     const hasLetters = /[a-zA-Z]/.test(word);
     const hasNumbers = /[\d]/.test(word);
     const isEmail = word.includes("@");
+    if (isEmail) {
+      result.categories = ["email"];
+      return result;
+    }
+    if (hasLetters) {
+      result.categories = ["email", "name"];
+    } else {
+      result.categories = ["phone"];
+    }
+    if (hasNumbers) {
+      result.categories = result.categories.filter((c) => c != "name");
+    } else {
+      result.categories = result.categories.filter((c) => c != "phone");
+    }
+    return result;
   };
   return (
     <div
